8a5d6a74833d423694bd00ddf6cdc63c11a16063
jacky-ui <jacky.du.004@gmail.com>
2024-12-03 15:15:01 -0500

    change links

--- src/components/Section2/Section2.js
+++ src/components/Section2/Section2.js
@@ -13,22 +13,25 @@ function Section2() {
             <article className='helpSection__icons'>
                 <StepIcons src={donation} alt='' header='Make a donation' 
                 text={
-                    <>
-                        <a href="/assets/Programming.pdf" aria-label="Click here to signup for our newsletter" target="_blank" rel="noopener noreferrer">Your gift goes directly to empowering young women and girls through free programming.</a>
+                    <>  
+                        Your gift goes directly to empowering young women and girls through free programming.{' '}
+                        <a href="/assets/Programming.pdf" aria-label="Click here to learn about our programming" target="_blank" rel="noopener noreferrer">Learn about our programming here.</a>
                     </>
                 }
                 />
                 <StepIcons src={spreadWord} alt='' header='Spread the Word' 
                     text={
                         <>
-                            <a href="https://forms.monday.com/forms/5a88e7f55f2ed83466b7733e8cefa525?r=apse2" aria-label="Click here to signup for our newsletter">Share our mission with your friends, family, and network.</a>
+                            Share our mission with your friends, family, and network.{' '}
+                            <a href="https://forms.monday.com/forms/5a88e7f55f2ed83466b7733e8cefa525?r=apse2" aria-label="Click here to signup for our newsletter">Signup for newsletter</a>
                         </>
                     }
                 />
                 <StepIcons src={volunteer} alt='' header='Volunteer or Partner' 
                     text={
                         <>
-                            <a href="https://forms.monday.com/forms/cbd05420e5d88509fc5426decf7d51a2?r=apse2" aria-label="Click here to volunteer">Get involved and make a lasting impact.</a>
+                            Get involved and make a lasting impact.{' '}
+                            <a href="https://forms.monday.com/forms/cbd05420e5d88509fc5426decf7d51a2?r=apse2" aria-label="Click here to volunteer">Get involved now</a>
                         </>
                     }
                 />
