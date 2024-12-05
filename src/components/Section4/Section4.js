d38196500e3615bbfb59d00c59c7bfbcbe07521f
jacky-ui <jacky.du.004@gmail.com>
2024-12-04 20:12:40 -0500

    section4 looks gtg

--- src/components/Section4/Section4.js
+++ src/components/Section4/Section4.js
@@ -1,5 +1,5 @@
 import Image from '../Image/Image';
-import ripple from '../../assets/images/ripple.png'
+import supportImage from '../../assets/images/support image.png';
 import './Section4.scss';
 
 function Section4() {
@@ -11,7 +11,7 @@ function Section4() {
                     <h2 className='rippleSection__heading'>THE RIPPLE EFFECT<br /> OF EMPOWERMENT<br /> IS PRICELESS.</h2>
                     <p className='rippleSection__text'>empowHERto is a women-led organization with the goal of positively impacting and uplifting minority and women girls through free life skills programming with a focus on financial literacy, leadership skills, mentorship and mental health.<br/><br/> At empowHERto, we’re dedicated to empowering young minority women and girls between the ages of 14 and 21 by providing them with the skills and resources to thrive, overcome systemic barriers and beat the statistics stacked against them.</p>
                 </div>
-                <Image src={ripple} alt=''/>
+                <Image src={supportImage} alt=''/>
                 </article>
             </div>
         </section>
