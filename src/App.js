b5ff801f8777bf304dac57efe63fd3317d960c3b
jacky-ui <jacky.du.004@gmail.com>
2024-12-13 05:16:47 -0500

    text change in hero

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Header from './components/Header/Header';
 import Image from './components/Image/Image';
-import donor from './assets/images/donors.png';
 import Section1 from './components/Section1/Section1';
 import Section2 from './components/Section2/Section2';
 import Section3 from './components/Section3/Section3';
-import './styles/App.css';
 import Section4 from './components/Section4/Section4';
 import Footer from './components/Footer/Footer';
+import Donors from './components/Donors/Donors';
+import donor from './assets/images/donors.png';
+import './styles/App.css';
 
 function App () {
   return(
     <>
       <Header />
       <main className='main'>
+        {/*<Donors />*/}
         <Image src={donor} alt='Our donors include: TD, Borden Ladner Gervais, datacamp, CIBC, IPSEN'/>
         <Section1 />
         <Section2 />
--- src/components/Donors/Donors.js
+++ src/components/Donors/Donors.js
@@ -1,9 +1,57 @@
 import './Donors.scss';
+import tdLogo from '../../assets/images/Toronto-Dominion_Bank_logo 1.png';
+import cibcLogo from '../../assets/images/CIBC_logo_2021 1.png';
+import datacampLogo from '../../assets/images/datacamp.png';
+import localityLogo from '../../assets/images/locality.png';
+import oipLogo from '../../assets/images/OIP 1.png';
+import ipsenLogo from '../../assets/images/Ipsen_logo 1.png';
+import blgLogo from '../../assets/images/blg.png';
+import Image from '../Image/Image.js';
 
 function Donors() {
+    const logos = [
+        {
+            logo: datacampLogo,
+            alt: 'Datacamp',
+        },
+        {
+            logo: cibcLogo,
+            alt: 'CIBC',
+        },
+        {
+            logo: localityLogo,
+            alt: 'Locality Bank',
+        },
+        {
+            logo: tdLogo,
+            alt: 'TD BANK',
+        },
+        {
+            logo: ipsenLogo,
+            alt: 'IPSEN Innovation for patient care',
+        },
+        {
+            logo: oipLogo,
+            alt: 'Community Foundation of Broward',
+        },
+        {
+            logo: blgLogo,
+            alt: 'Borden Ladner Gervais',
+        }
+    ]
     return(
-        <>
-        </>
+        <section className='donorLogos'>
+            <ul className='donorLogos__lists'>
+                {logos.map((logo) => (
+                    <li key={logo.logo}>
+                        <Image 
+                            src={logo.logo}
+                            alt={logo.alt}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </section>
     )
 };
 
