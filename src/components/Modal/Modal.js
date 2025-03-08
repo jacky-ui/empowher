a148e186c60238c5e37f46ae37ead0f8a603c397
jacky-ui <jacky.du.004@gmail.com>
2025-03-08 18:30:12 -0500

    set react state for modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,21 @@
 import './Modal.scss';
 import popupImage from '../../assets/images/popup-image.png';
+import { useState } from 'react';
+
 
 function Modal() {
+    const [isModalEnabled, setIsModalEnabled] = useState(false);
     return(
-        <>
-            <dialog className='popupEvent'>
+        <dialog className='popupEvent'>
+            <div>
                 <div>
                     <p>Join us for our 25 to BuildHER event</p>
                     <a href="https://www.zeffy.com/en-CA/ticketing/empowherto-presents-25-to-build-her">Join now</a>
                 </div>
                 <img src={popupImage} alt=''/>
-            </dialog>
-        </>
+            </div>
+            <button className='popupEvent--close'>X</button>
+        </dialog>
     )
 };
 
