2447af66b8add371a058d23574b933f75af17a21
jacky-ui <jacky.du.004@gmail.com>
2025-04-02 12:50:37 -0400

    commit out button for toronto

--- src/components/Section3/Section3.js
+++ src/components/Section3/Section3.js
@@ -9,7 +9,7 @@ function Section3() {
             <div>
                 <h2>Donate to either or both of our organizations</h2>
                 <div className='donateSection__links'>
-                    <Links href='https://www.zeffy.com/donation-form/25-to-build-her-giving-tuesday' text='To Toronto' aria='Click here to donate to Toronto location'/>
+                    {/*<Links href='https://www.zeffy.com/donation-form/25-to-build-her-giving-tuesday' text='To Toronto' aria='Click here to donate to Toronto location'/>*/}
                     <Links href='https://givebutter.com/25toBuildHER' text='To Fort Lauderdale' aria='Click here to donate to Fort Lauderdale'/>
                 </div>
             </div>
